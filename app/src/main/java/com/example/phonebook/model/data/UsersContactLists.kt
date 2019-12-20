a834d884eeb0f82794a94cae0f0bab5c33caf643
Oleg <lega932@yahoo.com>
2019-12-20 17:01:44 +0200

    Start kotlin migration

---
+++ app/src/main/java/com/example/phonebook/model/data/UsersContactLists.kt
@@ -0,0 +1,18 @@
+package com.example.phonebook.model.data
+
+import androidx.annotation.NonNull
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "usersContactLists")
+data class UsersContactLists (
+        @PrimaryKey
+        @ColumnInfo(name = "user_login")
+        @NonNull
+        var userName: String = "",
+        @ColumnInfo(name = "user_contact_list")
+        @NonNull
+        var contacts: String = ""
+)
+
